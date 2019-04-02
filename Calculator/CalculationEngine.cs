34b0309aeb50b3b6f8c1d2c8505dd13083e4fb63
Nathan Gonzalez <ncgonz@gmail.com>
2019-04-01 21:32:34 -0500

    adding some tdd testing examples

---
+++ Calculator/CalculationEngine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    public class CalculationEngine
+    {
+        public int DoMath(string stuffToCalculate)
+        {
+            var splitNumbers = stuffToCalculate.Split('+')
+                .Select(int.Parse).ToArray();
+
+            return splitNumbers[0] + splitNumbers[1];
+        }
+    }
+}
